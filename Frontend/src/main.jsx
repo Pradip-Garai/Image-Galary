f38c9e80fa725a3e0e4d376358d886fa8f455b33
Pradip Garai <studytimes116@gmail.com>
2025-06-07 11:19:25 +0530

    changes

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import 'react-toastify/ReactToastify.css'
+import 'react-toastify/dist/ReactToastify.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 
